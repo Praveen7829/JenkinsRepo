d5c1532a566e07713aac4fb113311b5e78d24ce4
PRAVEENKUMAR REDDY <PRAVEENKUMAR REDDY@LAPTOP-9245AL8D>
2022-12-05 16:25:09 +0530

    commit

---
+++ src/test/java/PracticeJenkins/Build.java
@@ -0,0 +1,11 @@
+package PracticeJenkins;
+
+import org.testng.annotations.Test;
+
+public class Build {
+
+	@Test
+	public void cret() {
+		System.out.println("buid created");
+	}
+}
---
+++ src/test/java/PracticeJenkins/BuildCreation.java
@@ -0,0 +1,17 @@
+package PracticeJenkins;
+
+import org.testng.annotations.Test;
+
+public class BuildCreation {
+
+	@Test
+	public void create() {
+		
+		System.out.println("build created");
+	}
+	
+	
+	public void delete() {
+		System.out.println("build not created");
+	}
+}
